600403d38f3bda23b94b6f791551eb504d685a49
Kota <maedakoutaa@gmail.com>
2022-08-14 15:53:33 +0900

    Initial Commit

---
+++ MiniApp85-SwiftUI-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp85-SwiftUI-List
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp85-SwiftUI-List/MiniApp85_SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp85_SwiftUI_ListApp.swift
+//  MiniApp85-SwiftUI-List
+//
+//  Created by 前田航汰 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp85_SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
